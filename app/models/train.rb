668db454f519a74e15255204c6a2cd2b0351605f
Denis B <hawk911@yandex.ru>
2016-06-12 17:55:18 +0300

    test sublimegit

--- app/models/train.rb
+++ app/models/train.rb
@@ -3,6 +3,6 @@ class Train < ActiveRecord::Base
 
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   belongs_to :route
-
+  # test
   has_many :tikets
 end
