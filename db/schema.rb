794cb8400c3101fd6864883733cb2bfbbb5f74b7
Denis B <hawk911@yandex.ru>
2016-06-20 21:33:43 +0300

    migration railway station

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618101044) do
+ActiveRecord::Schema.define(version: 20160620182708) do
 
   create_table "carriage_types", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160618101044) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "number"
   end
 
   create_table "railway_stations_routes", force: :cascade do |t|
