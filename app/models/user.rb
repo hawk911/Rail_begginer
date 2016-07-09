d6a75975e141898ab800f0318f768c11c557ebd7
Denis B <hawk911@yandex.ru>
2016-07-09 19:33:01 +0300

    task17

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable, :trackable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :tikets
+
   CURRENT_USER = User.find_by(name: 'Admin') || User.create(name: 'Admin')
 
-  validates :name, presence: true
+  #validates :name, presence: true
 
   def self.current_user
     CURRENT_USER
